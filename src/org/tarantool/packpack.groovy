f9c8d753cef4cd34a9ab1afcad165da079bcbe09
Konstantin Nazarov <mail@racktear.com>
2017-02-08 12:31:52 +0300

    Pipeline library for packpack parallel builds

---
+++ src/org/tarantool/packpack.groovy
@@ -0,0 +1,76 @@
+package org.tarantool;
+import groovy.transform.Field
+
+@Field List default_matrix = [
+    [OS: 'el', DIST: '6', PACK: 'rpm'],
+    [OS: 'el', DIST: '7', PACK: 'rpm'],
+    [OS: 'fedora', DIST: '23', PACK: 'rpm'],
+    [OS: 'fedora', DIST: '24', PACK: 'rpm'],
+    [OS: 'fedora', DIST: '25', PACK: 'rpm'],
+    [OS: 'fedora', DIST: 'rawhide', PACK: 'rpm'],
+    [OS: 'ubuntu', DIST: 'precise', PACK: 'deb'],
+    [OS: 'ubuntu', DIST: 'trusty', PACK: 'deb'],
+    [OS: 'ubuntu', DIST: 'xenial', PACK: 'deb'],
+    [OS: 'ubuntu', DIST: 'yakkety', PACK: 'deb'],
+    [OS: 'debian', DIST: 'jessie', PACK: 'deb'],
+    [OS: 'debian', DIST: 'stretch', PACK: 'deb'],
+]
+
+
+def packpackBuildClosure(src_stash, params) {
+    return {
+        node {
+            ws {
+                deleteDir()
+                unstash src_stash
+                dir('packpack') {
+                    git url: 'https://github.com/packpack/packpack.git'
+                }
+                sh 'git submodule update --init'
+
+                withEnv(["OS=${params['OS']}",
+                         "DIST=${params['DIST']}",
+                         "PACK=${params['PACK']}"]) {
+                    sh 'echo building on $OS $DIST'
+                    sh './packpack/packpack'
+                }
+
+                dst_stash = "${src_stash}-result-${params['OS']}-${params['DIST']}"
+                dir('build') {
+                    stash dst_stash
+                }
+            }
+        }
+    }
+}
+
+def packpackBuildMatrix(dst_stash, matrix=default_matrix) {
+    def stepsForParallel = [:]
+
+    src_stash = 'packpack-source'
+    stash name: src_stash, useDefaultExcludes: false
+
+    for (int i = 0; i < matrix.size(); i++) {
+        def params = matrix.get(i)
+        def stepName = "${params['OS']}-${params['DIST']}"
+        stepsForParallel[stepName] = packpackBuildClosure(src_stash, params)
+    }
+
+    parallel stepsForParallel
+
+    node {
+        ws {
+            deleteDir()
+            for (int i = 0; i < matrix.size(); i++) {
+                def params = matrix.get(i)
+                def stash_name = "${src_stash}-result-${params['OS']}-${params['DIST']}"
+                def dir_name = "${params['OS']}-${params['DIST']}"
+                dir(dir_name) {
+                    unstash stash_name
+                }
+            }
+            stash dst_stash
+        }
+    }
+
+}
